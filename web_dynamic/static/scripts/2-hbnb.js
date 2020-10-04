933a3ecd21cdcf85e2c14e11d0bc0e44e40688c0
Nic Basilio <bnbasilio@gmail.com>
2020-10-04 22:50:36 +0000

    re-write of 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,27 +1,30 @@
+const $ = window.$;
+const amenityDict = {};
 $(document).ready(function () {
-  const amenities = {};
-
-  $('input:checkbox').click(function () {
-    $(this).each(function () {
-      if (this.checked) {
-        amenities[$(this).data('id')] = $(this).data('name');
-      } else {
-        delete amenities[$(this).data('id')];
-      }
-    });
-    if (Object.values(amenities).length > 0) {
-      $('.amenities h4').text(Object.values(amenities).join(', '));
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
     } else {
-      $('.amenities h4').html('&nbsp');
+      $('#api_status').removeClass('available');
     }
   });
-});
-
-$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
-
-  if (data.status === 'OK') {
-    $('DIV#api_status').addClass('available');
-  } else {
-    $('DIV#api_status').removeClass('available');
-  }
+  $('li input').click(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').empty();
+    const amenityDictKeys = Object.keys(amenityDict);
+    const amenityDictLength = amenityDictKeys.length;
+    amenityDictKeys.forEach(function (key, index) {
+      $('.amenities h4').append(amenityDict[key]);
+      if (index !== amenityDictLength - 1) {
+        $('.amenities h4').append(', ');
+      }
+    });
+    if (amenityDictLength === 0) {
+      $('.amenities h4').append('&nbsp;');
+    }
+  });
 });
